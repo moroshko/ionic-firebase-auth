9314f9afb71c71bd38ec655fb48a7beee1b7c222
Misha Moroshko <michael.moroshko@gmail.com>
2014-07-21 22:00:12 +1000

    Bring higher level files

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,38 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var concat = require('gulp-concat');
+var karma = require('karma').server;
+var karmaConfig = require('./karma.js');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass:    ['./www/**/*.scss'],
+  scripts: ['./www/**/*.js', '!./www/lib/**/*.js', '!./www/myapp.js']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('jshint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
+gulp.task('test', function(done) {
+  karma.start(karmaConfig, done);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./www/ionic.app.scss')
+    .pipe(sass({
+      errLogToConsole: true,
+      includePaths: []
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    .pipe(concat('myapp.css'))
+    .pipe(gulp.dest('./www/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.scripts, ['jshint', 'test']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+gulp.task('default', ['jshint', 'test', 'sass', 'watch']);
