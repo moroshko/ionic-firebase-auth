e340c38e69602bb9a82541f34f33bfe540ea2098
Misha Moroshko <michael.moroshko@gmail.com>
2014-07-21 21:52:20 +1000

    Change starter to MyApp

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('MyApp.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('MyApp', ['ionic', 'MyApp.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
