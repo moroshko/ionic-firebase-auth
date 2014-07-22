a428ed9b42f4570ba147c9c19b232639dcd378ef
Misha Moroshko <michael.moroshko@gmail.com>
2014-07-22 23:09:26 +1000

    Login fallback

--- www/app.js
+++ www/app.js
@@ -28,7 +28,7 @@ angular.module('MyApp', [
 
   $urlRouterProvider.otherwise('/login');
 })
-.run(function($ionicPlatform) {
+.run(function($rootScope, $state, $ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory 
     // bar above the keyboard for form inputs)
@@ -40,6 +40,10 @@ angular.module('MyApp', [
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    $rootScope.$on('$stateChangeError', function() {
+      $state.go('login');
+    });
   });
 })
 .constant('FIREBASE_ROOT', 'https://myapp.firebaseio.com');
